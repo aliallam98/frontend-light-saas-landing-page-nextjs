c6aed8c4e31bd64eb82abc2c368a83f9654c868d
aliallam98 <allam98.ali@gmail.com>
2024-09-18 02:12:24 +0300

    Finished LogoTicker  section .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header";
 import Hero from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTicker />
     </>
   );
 }
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <section className=" p-8 md:p-12 bg-white">
+      <div className="container flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+        <div className="flex items-center gap-16 ">
+          <Image src={acmeLogo} alt="acme logo" width={120} height={30} />
+          <Image src={quantumLogo} alt="quantum Logo" width={120} height={30} />
+          <Image src={celestialLogo} alt="celestial Logo" width={120} height={30} />
+          <Image src={pulseLogo} alt="pulse Logo" width={120} height={30} />
+          <Image src={apexLogo} alt="apex Logo" width={120} height={30} />
+          <Image src={echoLogo} alt="echo Logo" width={120} height={30} />
+        </div>
+      </div>
+    </section>
+  );
 };
